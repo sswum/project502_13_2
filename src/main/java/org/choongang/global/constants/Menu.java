a32ecbb0582df1fa847a13932501c92214eec918
sswum <l5450won@naver.com>
2024-05-27 11:35:43 +0900

    라우터와 메뉴 만들기!

---
+++ src/main/java/org/choongang/global/constants/Menu.java
@@ -0,0 +1,10 @@
+package org.choongang.global.constants;
+
+public enum Menu {
+    MAIN, // 메인 화면
+    JOIN, // 회원가입
+    LOGIN, // 로그인
+    MYPAGE, //마이페이지
+
+    //상수를 추가해주고 거기에 따라서 필요한 것 내용 넣기(?)
+}
