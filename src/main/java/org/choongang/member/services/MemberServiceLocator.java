7408fc10c8569c74ae56e226c82b496d16bebd5d
sswum <l5450won@naver.com>
2024-05-27 16:41:51 +0900

    회원 가입 기능 구현

---
+++ src/main/java/org/choongang/member/services/MemberServiceLocator.java
@@ -0,0 +1,26 @@
+package org.choongang.member.services;
+
+import org.choongang.global.AbstractServiceLocator;
+import org.choongang.global.Service;
+import org.choongang.global.ServiceLocator;
+import org.choongang.global.constants.Menu;
+
+public class MemberServiceLocator implements AbstractServiceLocator {
+    private
+
+    @Override
+    public Service find(Menu menu) {
+        Service service =services.get(menu);
+        if (service != null) {
+            return service;
+        }
+        switch (menu) {
+            case JOIN: service = new JoinService(); break;
+            case LOGIN: service = new LoginService(); break;
+
+
+        }
+
+        return service;
+    }
+}
